cd8748a5118f3fc66fb5fe49bc9056adf4308306
Sasha Semenenko <semenenko233249@gmail.com>
2023-04-09 19:07:37 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -2,13 +2,15 @@ import os
 
 
 def move_file(command: str) -> None:
-    command_flag, first_file, second_file = command.split()
+    command_flag = command[0]
+    first_file = command[1]
+    second_file = command[2]
     if command_flag == "mv" and len(command.split()) == 3:
         directory, new_file = os.path.split(second_file)
         if directory:
             os.makedirs(directory, exist_ok=True)
         new_directory = os.path.join(directory, new_file)
-        with open(first_file, "r") as recent_file, \
-                open(new_directory, "w") as upgrade_file:
+        with (open(first_file, "r") as recent_file,
+              open(new_directory, "w") as upgrade_file):
             upgrade_file.write(recent_file.read())
         os.remove(first_file)
